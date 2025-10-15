53bb9e2ba946a6b64e0b3d01644a9f66773e4ec9
jiteshmunda <jitesh.munda@piquota.com>
2025-10-16 02:14:44 +0530

    CORS render

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -4,11 +4,22 @@ const corsOptions = {
   origin: (origin, callback) => {
     const allowedOrigins = [
       "https://nextgenlearn.magiqspark.ai",
+      "https://tinymagiq.yoweb.fun",
+      "https://tinymagiq-backend-f1tn.onrender.com",
       "http://localhost:3000",
       "http://localhost:3001",
+      "127.0.0.1", // Allow Nginx internal connection
+      "127.0.0.1:5000", // Allow Nginx internal connection with port
+      "localhost", // Allow Nginx internal connection without port
     ];
     if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, origin || "https://nextgenlearn.magiqspark.ai");
+      callback(
+        null,
+        origin ||
+          "https://nextgenlearn.magiqspark.ai" ||
+          "https://tinymagiq.yoweb.fun" ||
+          "https://tinymagiq-backend-f1tn.onrender.com"
+      );
     } else {
       callback(new Error(`CORS error: Origin ${origin} not allowed`));
     }
